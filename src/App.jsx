269a510c621f85be1e11c7d997db6d4dad3496bd
Vinícius Bastos <viniciusbsilva57@gmail.com>
2023-08-16 11:31:19 -0300

    Create CPF validation function

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,94 @@
-import { Component } from "react";
 import "./App.css";
-import FormularioCadastro from "./components/Formulario";
+import { Component } from "react";
+import FormularioCadastro from "./components/FormularioCadastro";
+import { Container, Typography } from "@mui/material";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 class App extends Component {
     render() {
         return (
-            <>
-                <h1>Formulário de cadastro</h1>
-                <FormularioCadastro />
-            </>
+            <Container component="article" maxWidth="sm">
+                <Typography variant="h3" align="center">
+                    Formulário de cadastro
+                </Typography>
+                <FormularioCadastro
+                    validarCPF={validarCPF}
+                    aoEnviar={aoEnviarFormulario}
+                />
+            </Container>
         );
     }
+}
+
+function aoEnviarFormulario(dados) {
+    console.log(dados);
+}
+
+function validarCPF(CPF) {
+    if (CPF.length < 11) {
+        return { cpf: { valido: false, texto: "CPF deve conter 11 dígitos" } };
+    }
+    if (
+        validaNumerosRepetidos(CPF) ||
+        validaPrimeiroDigito(CPF) ||
+        validaSegundoDigito(CPF)
+    ) {
+        return { cpf: { valido: false, texto: "CPF Inválido" } };
+    }
+
+    return { cpf: { valido: true, texto: "" } };
+}
+
+function validaNumerosRepetidos(CPF) {
+    const numerosRepetidos = [
+        "00000000000",
+        "11111111111",
+        "22222222222",
+        "33333333333",
+        "44444444444",
+        "55555555555",
+        "66666666666",
+        "77777777777",
+        "88888888888",
+        "99999999999",
+    ];
+
+    return numerosRepetidos.includes(CPF);
+}
+
+function validaPrimeiroDigito(cpf) {
+    let soma = 0;
+    let multiplicador = 10;
+
+    for (let tamanho = 0; tamanho < 9; tamanho++) {
+        soma += cpf[tamanho] * multiplicador;
+        multiplicador--;
+    }
+    soma = (soma * 10) % 11;
+
+    if (soma == 10 || soma == 11) {
+        soma = 0;
+    }
+    return soma != cpf[9];
+}
+
+function validaSegundoDigito(cpf) {
+    let soma = 0;
+    let multiplicador = 11;
+
+    for (let tamanho = 0; tamanho < 10; tamanho++) {
+        soma += cpf[tamanho] * multiplicador;
+        multiplicador--;
+    }
+    soma = (soma * 10) % 11;
+
+    if (soma == 10 || soma == 11) {
+        soma = 0;
+    }
+    return soma != cpf[10];
 }
 
 export default App;
---
+++ src/components/FormularioCadastro/index.jsx
@@ -0,0 +1,90 @@
+import { Button, FormControlLabel, Switch, TextField } from "@mui/material";
+import { useState } from "react";
+
+export default function FormularioCadastro({ aoEnviar, validarCPF }) {
+    const [nome, setNome] = useState("");
+    const [sobrenome, setSobrenome] = useState("");
+    const [cpf, setCpf] = useState("");
+    const [promocoes, setPromocoes] = useState(true);
+    const [novidades, setNovidades] = useState(false);
+    const [erros, setErros] = useState({ cpf: { valido: true, texto: "" } });
+
+    return (
+        <form
+            onSubmit={(event) => {
+                event.preventDefault();
+                aoEnviar({ nome, sobrenome, cpf, promocoes, novidades });
+            }}
+        >
+            <TextField
+                value={nome}
+                onChange={(event) => {
+                    setNome(event.target.value);
+                }}
+                id="nome"
+                label="Nome"
+                variant="filled"
+                fullWidth
+                margin="normal"
+            />
+
+            <TextField
+                value={sobrenome}
+                onChange={(event) => {
+                    setSobrenome(event.target.value);
+                }}
+                id="sobrenome"
+                label="Sobrenome"
+                variant="filled"
+                fullWidth
+                margin="normal"
+            />
+
+            <TextField
+                value={cpf}
+                onChange={(event) => {
+                    setCpf(event.target.value);
+                }}
+                onBlur={() => {
+                    setErros(validarCPF(cpf));
+                }}
+                error={!erros.cpf.valido}
+                helperText={erros.cpf.texto}
+                id="CPF"
+                label="CPF"
+                variant="filled"
+                fullWidth
+                margin="normal"
+            />
+
+            <FormControlLabel
+                label="Promoções"
+                control={
+                    <Switch
+                        onChange={(event) => {
+                            setPromocoes(event.target.checked);
+                        }}
+                        name="promocoes"
+                        checked={promocoes}
+                    />
+                }
+            />
+            <FormControlLabel
+                label="Novidades"
+                control={
+                    <Switch
+                        onChange={(event) => {
+                            setNovidades(event.target.checked);
+                        }}
+                        name="novidades"
+                        checked={novidades}
+                    />
+                }
+            />
+
+            <Button type="submit" variant="contained" color="primary">
+                Cadastrar
+            </Button>
+        </form>
+    );
+}
